7cd82b59a0c1f4a919f77340eb6d90afa145dda9
Utanu <mpotter2782@gmail.com>
2016-05-28 20:27:34 -0500

    changes, set up database documents added

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 
-DEFINE("DB_USER","melinda");
-DEFINE("DB_PASSWORD","monkeylove");
-DEFINE("DB_HOST","host");
+DEFINE("DB_USER","root");
+DEFINE("DB_PASSWORD","AnimE9009");
+DEFINE("DB_HOST","localhost");
 DEFINE("DB_NAME","inventory");
 
-$dbc = @mysqli_connect(DB_UDER, DB_PASSWORD, DB_HOST, DB_NAME)
+$dbc = @mysqli_connect(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 OR die("Could not connect to database" .
 		mysqli_connect_error());
 ?>
